397e6f1886c1ec1d4effcfdfc59ee25179fa716f
Marta Kulig <bialka104b@interia.pl>
2021-04-06 20:22:06 +0200

    add process.env.PORT

--- index.js
+++ index.js
@@ -3,24 +3,18 @@ const express = require("express");
 const handle_bars = require("express-handlebars");
 const app = express();
 
-app.engine("handlebars", handle_bars({defaultLayout: "main"}));//głównym plikiem w którym jest szablon strony bedzie main.handlebars
-app.set("view engine", "handlebars");//uzywam handlebarsów
+app.engine("handlebars", handle_bars({ defaultLayout: "main" })); //głównym plikiem w którym jest szablon strony bedzie main.handlebars
+app.set("view engine", "handlebars"); //uzywam handlebarsów
 
-app.use( express.static("public") );
+app.use(express.static("public"));
 
-
-
-app.get("/", function(req, res) {
-
-    res.render("home", {
-        title: "tytuł strony",
-        content: "kotent strony"
-    });
-
+app.get("/", function (req, res) {
+  res.render("home", {
+    title: "tytuł strony",
+    content: "kotent strony",
+  });
 });
 
-app.listen(8080, function() {
-
-    console.log("Serwer został uruchomiony pod adresem http://localhost:8080");
-
+app.listen(process.env.PORT || 8080, function () {
+  console.log("Serwer został uruchomiony pod adresem http://localhost:8080");
 });
